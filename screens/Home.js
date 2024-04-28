0dbd1b956d2d5a799530df36b47db4fcaea903bb
Sarthak Gudwani <gudwanisarthak2000@gmail.com>
2024-04-28 21:02:21 +0530

    Added Restaurant Item Component integrated with Yelp API

--- screens/Home.js
+++ screens/Home.js
@@ -1,17 +1,41 @@
-import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, StyleSheet, ScrollView } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import HeaderTabs from '../components/HeaderTabs'
 import SearchBar from '../components/SearchBar';
 import Categories from '../components/Categories';
+import RestaurantItems, { localRestaurants } from '../components/RestaurantItems';
+
+const YELP_API_KEY = "dyiaIx9L13sgaDePN9HRRXP68VtZpYVLGcWmRyp8hLLQ6FBsG87LwJhaT1NAd6spVw2_lntGHlTV2zsfpN7vcDliyhCP9hnuxzCGvpu7piCbT7J6X2qFNwxVVV4uZnYx";
 
 export default function Home() {
-  return (
-    <SafeAreaView style={{backgroundColor:"#eee", flex:1}}>
-      <View style={{backgroundColor:"white", padding:15, paddingTop:50}}>
-        <HeaderTabs />
-        <SearchBar />
-      </View>
-      <Categories />
-   </SafeAreaView>
-  );
+
+    const [restaurantData, setRestaurantData] = useState(localRestaurants);
+
+    const getRestaurantsFromYelp = () => {
+        const yelpurl = `https://api.yelp.com/v3/businesses/search?term=restaurants&location=san%20francisco`;
+
+        const apiOptions = {
+            headers: {
+                Authorization: `Bearer ${YELP_API_KEY}`
+            },
+        };
+
+        return fetch(yelpurl, apiOptions).then((res) => res.json()).then(json => setRestaurantData(json.businesses));
+    };
+
+    useEffect(() => {
+        getRestaurantsFromYelp();
+    }, [])
+    return (
+        <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
+            <View style={{ backgroundColor: "white", padding: 15, paddingTop: 50 }}>
+                <HeaderTabs />
+                <SearchBar />
+            </View>
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <Categories />
+                <RestaurantItems restaurantData={restaurantData} />
+            </ScrollView>
+        </SafeAreaView>
+    );
 }
