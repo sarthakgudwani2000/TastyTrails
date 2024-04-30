60e5a2274b023fc3063cbb167cc27ebc1ea119c1
Sarthak Gudwani <gudwanisarthak2000@gmail.com>
2024-04-30 16:20:02 +0530

    Added functionality for Header Tabs

--- components/HeaderTabs.js
+++ components/HeaderTabs.js
@@ -1,13 +1,12 @@
 import { View, Text, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
 
-export default function HeaderTabs() {
-    const [activeTab, setActiveTab] = useState("Delivery");
+export default function HeaderTabs(props) {
 
     return (
         <View style={{ flexDirection: "row", alignSelf: "center" }}>
-            <HeaderButton text="Delivery" btnColor="black" textColor="white" activeTab={activeTab} setActiveTab={setActiveTab} />
-            <HeaderButton text="Pickup" btnColor="white" textColor="black" activeTab={activeTab} setActiveTab={setActiveTab} />
+            <HeaderButton text="Delivery" btnColor="black" textColor="white" activeTab={props.activeTab} setActiveTab={props.setActiveTab} />
+            <HeaderButton text="Pickup" btnColor="white" textColor="black" activeTab={props.activeTab} setActiveTab={props.setActiveTab} />
         </View>
     );
 }
