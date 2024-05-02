5e591843c8a243ae85b21317744cf6406b7a623d
Sarthak Gudwani <gudwanisarthak2000@gmail.com>
2024-05-02 14:17:18 +0530

    Fixed BouncyCheckbox Bugs

--- components/restaurantDetail/ViewCart.js
+++ components/restaurantDetail/ViewCart.js
@@ -1,8 +1,20 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { TouchableOpacity } from 'react-native-gesture-handler'
+import { useSelector } from 'react-redux'
 
 export default function ViewCart() {
+    const items = useSelector((state) => state.cartReducer.selectedItems.items);
+    const total = items
+    .map((item) => Number(item.price.replace("$", "")))
+    .reduce((prev, curr) => prev + curr, 0);
+
+    const totalPrice = total.toLocaleString("en", {
+        style: "currency",
+        currency: "USD",
+      });
+    console.log(totalPrice);
+
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', flexDirection: 'row', position: 'absolute', bottom: 10, zIndex: 999, }}>
             <View style={{ flexDirection: 'row', justifyContent: 'center', width: "100%" }}>
